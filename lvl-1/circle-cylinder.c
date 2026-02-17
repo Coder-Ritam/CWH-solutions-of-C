393333ad369f8ec3badbeb10cc7fc80a5149717b
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-02-17 17:25:36 +0530

    Add files via upload

--- lvl-1/circle-cylinder.c
+++ lvl-1/circle-cylinder.c
@@ -3,10 +3,17 @@
 /*This program calculates area of a circle and cylinder*/
 
 int main(){
-    float radius,area;
+    float radius,area,area2,height;
     printf("enter radius of a circle: \n");
     scanf("%f",&radius);
     area=(22.0/7.0)*(radius*radius);
     printf("the area of a circle is %f\n",area);
+
+    //area of cylinder
+    printf("Enter height of cylinder: \n");
+    scanf("%f",&height);
+
+    area2=2.0*((22.0/7.0)*(radius*height));
+    printf("area of cylinder is %f\n",area2);
     return 0;
 }
