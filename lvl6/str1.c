958c56527867b3b3045e2ce01b784e2c1580ff0e
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-02-22 18:15:35 +0530

    Add : a string program
    Program to print characters of a string

--- lvl6/str1.c
+++ lvl6/str1.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+/*Program to create a string and print its characters*/
 
+int main()
+{
+    char s[] = "Ritam";
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        printf("%c\t", s[i]);
+    }
+    return 0;
+}
