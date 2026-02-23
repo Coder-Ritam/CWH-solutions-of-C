72a4966889ff7a809f1ab04c7ea15ac85500582a
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-02-23 18:26:38 +0530

    rm : unnecessary variables

--- lvl6/str4.c
+++ lvl6/str4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 //created a custom slice function to cut the string
 char* slice(char str[],int m,int n){
-    int i=0,count;
     char *ptr=&str[m];
     char *ptr2=&str[n];
     str=ptr;
@@ -11,6 +10,6 @@ char* slice(char str[],int m,int n){
 
 int main(){
     char str[]="Ritam Don";
-    printf("%s", slice(str,1,5));
+    printf("%s\n", slice(str,1,5));
     return 0;
 }
