d0bed710ee9820b3820249c4bbd91f123e7b33c6
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-02-24 20:03:02 +0530

    Add : occurence counter
    Program to find occurences of a given character in a word

---
+++ lvl6/str6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*Program to find occurences of a given character in a word*/
+
+int count(char find,char str[]);
+
+int main(){
+    char str[]="reetame";
+    char find='e';
+    /*int count=0;
+    int i=0;
+    while(str[i]!='\0'){
+        i++;
+        if(str[i]==find){
+            count++;
+        }
+    }*/
+    //printf("the number of occurences: %d\n",count);
+    int c=count(find,str);
+    printf("%d\n",c);
+    return 0;
+}
+
+int count(char find,char str[])
+{
+    int count=0;
+    int i=0;
+    while(str[i]!='\0'){
+        i++;
+        if(str[i]==find){
+            count++;
+        }
+}
+return count;
+}
