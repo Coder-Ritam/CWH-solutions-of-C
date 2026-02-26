4782f305ed78a7babb23bdc3ee8293f1416e00ff
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-02-26 20:02:32 +0530

    Add : program to find sum of two vectors

--- lvl7/st2.c
+++ lvl7/st2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/*Create a 2d vector using structures*/
+
+struct vector{
+    int x;
+    int y;
+};
+
+int main(){
+    struct vector v={1,2};
+    printf("the value  of vector is %dx +  %dy\n",v.x,v.y);
+    return 0;
+}
