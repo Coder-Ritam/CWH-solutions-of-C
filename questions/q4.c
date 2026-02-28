2d40e3261a4f47fecdf3014bd45dfb65a51df181
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-02-28 14:18:30 +0530

    Add : added new struct format
    Learning about structures from scratch part 1

--- questions/q4.c
+++ questions/q4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*Learning about structures from scratch part 1*/
+/*Learning about structures from scrach part 1*/
 struct Numbers{
     int num;
     float num2;
@@ -12,6 +12,9 @@ int main(){
     n1.num2=9;
     n2.num=9;
     n2.num2=10;
+    struct Numbers n3={1,2.0};
     printf("the first equation is %dx+%.1f \n",n1.num,n1.num2);
+    printf("the second equation is %dx+%.1f \n",n2.num,n2.num2);
+    printf("the third equation is %dx+%.1f \n",n3.num,n3.num2);
     return 0;
 }
