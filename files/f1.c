52828ff0df0c8ba236047401320718d3fff6ac9b
Coder-Ritam <chai.aur.ritam@gmail.com>
2026-03-03 20:25:57 +0530

    Add files via upload

--- files/f1.c
+++ files/f1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/*Write a program to check whether a file exits or not before opening it*/
+
+//void check(){
+//    if()
+//}
+
+int main(){
+    FILE *ptr;
+    ptr=fopen("ritam.txt","w");
+    fprintf(ptr,"Some text");
+    fclose(ptr);
+    ptr=fopen("ritam.txt","a");
+    fprintf(ptr,"\nHello\n");
+    fclose(ptr);
+    ptr=fopen("ritam.txt","r");
+    char St[100];
+    while(fgets(St,100,ptr)) printf("%s",St);
+    return 0;
+}
